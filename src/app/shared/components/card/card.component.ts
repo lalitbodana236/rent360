200140d44b1532a294b759fe61dbe5b199097bc9
lalitbodana236 <lalitbodana236@gmail.com>
2026-02-14 21:53:25 +0530

    UI issue fixes

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'r360-card',
+  host: { class: 'block' },
   template: '<div class="r360-card"><ng-content></ng-content></div>',
 })
 export class CardComponent {}
